d395d82deb6fe7c4ef1751a1b72ecc92a8c9c2e2
Matthew Leingang <leingang@nyu.edu>
2022-10-01 12:41:12 -0400

    Add versioning code to build file

--- ngelesson/build.lua
+++ ngelesson/build.lua
@@ -2,8 +2,17 @@
 
 bundle = "calc3-fall22"
 module = "ngelesson22-worksheet"
+version = "0.6"
+versiondate = "2022-09-26"
+
 maindir = ".."
 
+installfiles = {"*.sty", "*.cls"}
 typesetfiles  = {"*.tex"}
 
+tagfiles = {"*.dtx", "build.lua"}
 
+dofile(maindir .. "/config.lua")
+
+-- LaTeX style versioning
+next_version = next_version_latex
