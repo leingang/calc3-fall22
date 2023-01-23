ec14cb8b5213615a67601dc2ee60a1a9c0fee41f
Matthew Leingang <leingang@nyu.edu>
2023-01-22 21:47:10 -0500

    Add a module with a beamer inner theme

---
+++ beamer/build.lua
@@ -0,0 +1,18 @@
+#!/usr/bin/env texlua
+
+bundle = "calc3-fall22"
+module = "beamer"
+version = "0.0"
+versiondate = "2023-01-22"
+
+maindir = ".."
+
+installfiles = {"*.sty", "*.cls"}
+typesetfiles  = {"*.tex"}
+
+tagfiles = {"*.dtx", "build.lua"}
+
+dofile(maindir .. "/config.lua")
+
+-- LaTeX style versioning
+next_version = next_version_latex
